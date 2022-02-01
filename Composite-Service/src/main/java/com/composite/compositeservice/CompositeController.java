827bee77b7be6509704ec083b6d98c50f2880c9c
Aaron James <aaron.james@hcl.com>
2022-02-01 14:43:15 -0800

    version control

--- Composite-Service/src/main/java/com/composite/compositeservice/CompositeController.java
+++ Composite-Service/src/main/java/com/composite/compositeservice/CompositeController.java
@@ -16,14 +16,14 @@ public class CompositeController {
     @GetMapping("/destination/{destId}")
     public ResponseEntity<Composite> createComposite(@PathVariable long destId) {
         Composite comp = new Composite();
-        comp.setDestination(restTemplate.getForObject("http://localhost:8080/destinations/" + destId, Destination.class));
+        comp.destination = restTemplate.getForObject("http://localhost:8080/destinations/" + destId, Destination.class);
         long[] revIds = restTemplate.getForObject("http://localhost:8081/reviews/destination/" + destId, long[].class);
         for(int i = 0; i < revIds.length; i++) {
-            comp.addReview(restTemplate.getForObject("http://localhost:8081/reviews/" + revIds[i], Review.class));
+            comp.reviews.add(restTemplate.getForObject("http://localhost:8081/reviews/" + revIds[i], Review.class));
         }
         long[] recIds = restTemplate.getForObject("http://localhost:8082/recommend/destination/" + destId, long[].class);
         for(int i = 0; i < recIds.length; i++) {
-            comp.addRec(restTemplate.getForObject("http://localhost:8082/recommend/" + recIds[i], Recommend.class));
+            comp.recommendations.add(restTemplate.getForObject("http://localhost:8082/recommend/" + recIds[i], Recommend.class));
         }
         return ResponseEntity.ok().body(comp);
     }
