cfc96b278348bd974ca7e370440965bd34e993a1
Aaron James <aaron.james@hcl.com>
2022-02-10 16:00:40 -0800

    removing destinationservice to be replaced for troubleshooting

--- destinationservice/src/main/java/com/destination/destinationservice/Repository/DestinationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.destination.destinationservice.Repository;
-
-import com.destination.destinationservice.Entity.Destination;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinationRepo extends JpaRepository<Destination, Long>{
-    
-}
